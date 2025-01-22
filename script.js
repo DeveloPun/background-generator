8e39b1db8ecf5bd51492cc9bd5be89579caf1ece
Fethrauga <jeffreyshepson@gmail.com>
2025-01-22 14:54:00 -0600

    added npm packages

---
+++ script.js
@@ -0,0 +1,21 @@
+var _ = require('lodash');
+
+var array = [1,2,3,4,5,6,7,8,];
+console.log('answer:', _.without(array, 3))
+const css = document.querySelector('h3');
+const [color1, color2] = document.querySelectorAll('.color')
+const body = document.querySelector('#gradient');
+
+setGradient = () => {
+	body.style.background = _linearGradient();
+	css.textContent = `${body.style.background};`;
+}
+
+
+_linearGradient = () => {
+	return `linear-gradient(to right, ${color1.value}, ${color2.value})`;
+}
+
+color1.addEventListener('input', setGradient);
+
+color2.addEventListener('input', setGradient);
